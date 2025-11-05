785781454829456dfb3c2d65fe7cbd58d1454fed
JESUS GILBERTO RAMIREZ LIZARRAGA <gilbertoramirez@solemti.com>
2025-11-05 11:05:42 -0700

    fix: correccion de  importación y uso de imagen en LoadingSpinner

--- src/components/ui/LoadingSpinner.jsx
+++ src/components/ui/LoadingSpinner.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import dinosaurioGif from '../../assets/video/dinosaurio-34.gif'
 
 const LoadingSpinner = () => {
   return (
     <div className="fixed inset-0 bg-white bg-opacity-90 flex items-center justify-center z-50">
       <div className="text-center">
         <img 
-          src="/src/assets/video/dinosaurio-34.gif" 
+          src={dinosaurioGif}
           alt="Cargando..." 
           className="w-32 h-32 mx-auto mb-4"
         />
